b73b0378b107682d2e18f5cc0578d7d0ca73673f
Rao-Adi <64082788+Rao-Adi@users.noreply.github.com>
2023-07-07 00:55:20 +0500

    More guests

---
+++ Quater1/Assignment1/Ex-16-MoreGuests/main.ts
@@ -0,0 +1,28 @@
+let friendNames: String[] = ["Abid Ali","Sohail Ahmed","Umar Khattab"];
+//Print array item with invitation
+printInvitation(friendNames);
+
+console.log(`Hurra! We found a bigger table for dinner.\n`);
+ 
+//Adding friend at the beginning 
+friendNames.unshift("Owais");
+console.log(`After adding friend "Owais" in the beginning .\n`);
+printInvitation(friendNames);
+
+// Adding friend at index 2
+friendNames.splice(2,0,"Yahya Husaini");
+console.log(`After adding friend "Yahya Husaini" at Index 2 .\n`);
+printInvitation(friendNames);
+
+
+// Adding friend at the end
+friendNames.push("Muhammad Noman");
+console.log(`After adding friend "Muhammad Noman" at the end .\n`);
+printInvitation(friendNames);
+
+function printInvitation(arr:String[]){
+    arr.forEach(name => {
+        console.log(`Dear ${name}, You are cordially invited to attend the dinner party we have planned for June 7 2023 \n`);
+    })
+}
+
