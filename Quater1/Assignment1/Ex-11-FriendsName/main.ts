ef71959576cef2ed525a6a1ed01d6463741816b5
Rao-Adi <64082788+Rao-Adi@users.noreply.github.com>
2023-07-06 23:53:05 +0500

    Friend Names

---
+++ Quater1/Assignment1/Ex-11-FriendsName/main.ts
@@ -0,0 +1,6 @@
+let friendsName: String[] = ["Sohail Khan", "Owais Afsar", "Muhammad Hammad", "Yahya Husaini"];
+
+console.log("My Friend Names are: ")
+friendsName.forEach((name, index) => {
+    console.log(`${index + 1} :${name}`);
+})
