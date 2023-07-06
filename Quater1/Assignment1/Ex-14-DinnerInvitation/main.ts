d7314ba84bd3ae94cf7ea96f6a5703ca3229c67d
Rao-Adi <64082788+Rao-Adi@users.noreply.github.com>
2023-07-07 00:33:08 +0500

    Dinner Invitations to friends

---
+++ Quater1/Assignment1/Ex-14-DinnerInvitation/main.ts
@@ -0,0 +1,5 @@
+let friendNames: String[] = ["Abid Ali","Sohail Ahmed","Umar Khattab"];
+ 
+friendNames.forEach(name => {
+    console.log(`Dear ${name}, You are cordially invited to attend the dinner party we have planned for June 7 2023`);
+})
