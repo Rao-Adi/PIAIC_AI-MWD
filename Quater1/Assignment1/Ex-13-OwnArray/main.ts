beedf8bf068f91061c9a0a5b096029a6fd1b1ea1
Rao-Adi <64082788+Rao-Adi@users.noreply.github.com>
2023-07-07 00:28:21 +0500

    Own array with Vehicles name

---
+++ Quater1/Assignment1/Ex-13-OwnArray/main.ts
@@ -0,0 +1,5 @@
+let vehicleNames: String[] = ["Ford car", "Honda motorcycle", "BMW motorcycle", "BMW car","Mercedes‑Benz car","KTM motorcycle","Harley Davidson motorcycle"];
+ 
+vehicleNames.forEach(name => {
+    console.log(`I want to own a ${name}`);
+})
