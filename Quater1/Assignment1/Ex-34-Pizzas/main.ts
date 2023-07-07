a8c2e9ece72da13e739ca822c81da45bc39414ab
Rao-Adi <64082788+Rao-Adi@users.noreply.github.com>
2023-07-07 23:27:13 +0500

    Pizzas

---
+++ Quater1/Assignment1/Ex-34-Pizzas/main.ts
@@ -0,0 +1,7 @@
+let pizzas: string[] = ["Pepperoni", "Margherita", "Hawaiian"];
+
+for (let i = 0; i < pizzas.length; i++) {
+  console.log(`I like ${pizzas[i]} pizza.`);
+}
+
+console.log("I really love pizza!");
