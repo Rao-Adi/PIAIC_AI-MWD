58f71f35d676166af586ec2f427e59c8489ebc5f
Rao-Adi <64082788+Rao-Adi@users.noreply.github.com>
2023-07-07 18:00:41 +0500

    Alien Colors

---
+++ Quater1/Assignment1/Ex-25-AlienColors/main.ts
@@ -0,0 +1,13 @@
+let alien_color = 'green';
+
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points.");
+}
+
+
+//Second version
+alien_color = 'yellow';
+
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points.");
+}
