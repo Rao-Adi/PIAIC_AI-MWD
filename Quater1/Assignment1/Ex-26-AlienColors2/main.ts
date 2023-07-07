373da095422ea8d016081f228a2c0c0c66236947
Rao-Adi <64082788+Rao-Adi@users.noreply.github.com>
2023-07-07 18:20:44 +0500

    Alien Colors 2

---
+++ Quater1/Assignment1/Ex-26-AlienColors2/main.ts
@@ -0,0 +1,15 @@
+let alien_color = 'green';
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points for shooting the alien.");
+} else {
+  console.log("Congratulations! You just earned 10 points for shooting the alien.");
+}
+
+
+//Version 2
+alien_color = 'red';
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points for shooting the alien.");
+} else {
+  console.log("Congratulations! You just earned 10 points for shooting the alien.");
+}
