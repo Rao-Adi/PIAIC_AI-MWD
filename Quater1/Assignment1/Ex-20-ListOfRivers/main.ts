9ce78b6076f5a14be4a6c5d769fbeb3e9036311e
Rao-Adi <64082788+Rao-Adi@users.noreply.github.com>
2023-07-07 17:29:59 +0500

    List of Rivers

---
+++ Quater1/Assignment1/Ex-20-ListOfRivers/main.ts
@@ -0,0 +1,15 @@
+// Create an array to store rivers
+let rivers: string[] = [
+    "Nile",
+    "Amazon",
+    "Yangtze",
+    "Mississippi",
+    "Danube",
+    "Ganges",
+    "Congo"
+  ];
+  
+  // Print the array of rivers
+  console.log("List of rivers:");
+  console.log(rivers);
+  
