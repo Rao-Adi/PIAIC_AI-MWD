9f9b5957bd8ca68d81489822e106f01ced736c7d
Rao-Adi <64082788+Rao-Adi@users.noreply.github.com>
2023-07-07 17:26:25 +0500

    Dinner Guests count

---
+++ Quater1/Assignment1/Ex-19-DinnerGuestsCount/main.ts
@@ -0,0 +1,3 @@
+let friendNames: String[] = ["Abid Ali", "Sohail Ahmed", "Umar Khattab"];
+let guestCount = friendNames.length;
+console.log(`The number of people invited to dinner is ${guestCount}.`);
