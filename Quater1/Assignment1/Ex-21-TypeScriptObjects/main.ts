a537c6f16e0f858f70a664f6a46f01ccc32be757
Rao-Adi <64082788+Rao-Adi@users.noreply.github.com>
2023-07-07 17:38:25 +0500

    Typescript Objects about Rivers

---
+++ Quater1/Assignment1/Ex-21-TypeScriptObjects/main.ts
@@ -0,0 +1,28 @@
+// Create objects for different rivers
+let nile = {
+    name: "Nile",
+    length: 6650,
+    countries: ["Egypt", "Sudan", "Ethiopia", "Uganda", "Tanzania"],
+    discharge: 2830
+  };
+  
+  let amazon = {
+    name: "Amazon",
+    length: 6992,
+    countries: ["Brazil", "Peru", "Colombia"],
+    discharge: 209000
+  };
+  
+  let yangtze = {
+    name: "Yangtze",
+    length: 6300,
+    countries: ["China"],
+    discharge: 31900
+  };
+  
+  // Print the information about each river
+  console.log("Information about rivers:");
+  console.log("Nile:", nile);
+  console.log("Amazon:", amazon);
+  console.log("Yangtze:", yangtze);
+  
