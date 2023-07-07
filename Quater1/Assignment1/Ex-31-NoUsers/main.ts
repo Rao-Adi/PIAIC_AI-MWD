b03422debcbb3a80d23015b192a1b52d3b882f2c
Rao-Adi <64082788+Rao-Adi@users.noreply.github.com>
2023-07-07 18:42:23 +0500

    No Users

---
+++ Quater1/Assignment1/Ex-31-NoUsers/main.ts
@@ -0,0 +1,20 @@
+var usernames = ["admin", "Sohail", "Owais", "Murtaza", "Ali"];
+
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+  } else {
+    for (let i = 0; i < usernames.length; i++) {
+      if (usernames[i] === "admin") {
+        console.log("Hello admin, would you like to see a status report?");
+      } else {
+        console.log(`Hello ${usernames[i]}, thank you for logging in again.`);
+      }
+    }
+  }
+  
+  // Removing all usernames from the array
+  usernames = [];
+  
+  if (usernames.length === 0) {
+    console.log("We need to find some users!");
+  }
