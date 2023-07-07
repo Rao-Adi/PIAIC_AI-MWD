0766e6f98e7329a6541c25c3d28e9e6b031b79e2
Rao-Adi <64082788+Rao-Adi@users.noreply.github.com>
2023-07-07 23:34:16 +0500

    T-Shirts

---
+++ Quater1/Assignment1/Ex-36-T-Shirts/main.ts
@@ -0,0 +1,7 @@
+//Created function
+function make_shirt(size: string, message: string) {
+  console.log(`The shirt size is ${size} and it will have the message: "${message}" printed on it.`);
+}
+
+// Calling the function
+make_shirt("Large", "I Love Programming");
